2714905f5235941662b1116d5935b236f049567a
Sebastian Jeong <sebastianrcnt@gmail.com>
2021-07-20 08:40:16 +0000

    cool

--- malls/musinsa/product_crawler.py
+++ malls/musinsa/product_crawler.py
@@ -1,9 +1,10 @@
 import json
 import regex
-from utils import jsonprint
+from utils import jsonprint, timeit, timeit_async
 from bs4 import BeautifulSoup
 import asyncio
 import aiohttp
+import time
 
 headers = {
     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.106 Safari/537.36',
@@ -69,7 +70,9 @@ async def extract_musinsa_product_tag_list(bs):
   tags.append(brand)
   tags.append(category)
   return tags
+  
 async def crawl_musinsa_product_by_id(product_id):
+  start = time.time()
   print(f"start crawling {product_id}")
   async with aiohttp.ClientSession() as session:
     url = f'https://store.musinsa.com/app/goods/{product_id}'
@@ -95,10 +98,10 @@ async def crawl_musinsa_product_by_id(product_id):
       product['styled_image']=await extract_musinsa_product_image(bs)
       product['tag_list']=await extract_musinsa_product_tag_list(bs)
       product['item_type']="default"
-  print(f"end crawling {product_id}")
-  # print(product)
+  end = time.time()
+  print(f"end crawling {product_id}, elapsed time: {end-start}")
   return product
 
-#product = asyncio.run(crawl_musinsa_product_by_id('1891162'))
-#print(product)
+# product = asyncio.run(crawl_musinsa_product_by_id('1891162'))
+# print(product)
 # print(json.dumps(product, indent=2, ensure_ascii=False))
--- malls/musinsa/utils.py
+++ malls/musinsa/utils.py
@@ -1,4 +1,41 @@
 import json
+import time
 
 def jsonprint(data):
   print(json.dumps(data, ensure_ascii=False, indent=4))
+
+def timeit(method):
+    def timed(*args, **kw):
+        ts = time.time()
+        result = method(*args, **kw)
+        te = time.time()
+        if 'log_time' in kw:
+            name = kw.get('log_name', method.__name__.upper())
+            kw['log_time'][name] = int((te - ts) * 1000)
+        else:
+            print('%r  %2.2f ms' % \
+                  (method.__name__, (te - ts) * 1000))
+        return result
+    return timed
+
+def timeit_async(func):
+    async def process(func, *args, **params):
+        if asyncio.iscoroutinefunction(func):
+            print('this function is a coroutine: {}'.format(func.__name__))
+            return await func(*args, **params)
+        else:
+            print('this is not a coroutine')
+            return func(*args, **params)
+
+    async def helper(*args, **params):
+        print('{}.time'.format(func.__name__))
+        start = time.time()
+        result = await process(func, *args, **params)
+
+        # Test normal function route...
+        # result = await process(lambda *a, **p: print(*a, **p), *args, **params)
+
+        print('>>>', time.time() - start)
+        return result
+
+    return helper
